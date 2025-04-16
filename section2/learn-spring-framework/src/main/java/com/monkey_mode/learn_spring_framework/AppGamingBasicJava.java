1ad06b4b7603deb8f9875970d61ef756c01e4c57
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-16 22:19:10 +0700

    Add pacman game

--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/AppGamingBasicJava.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/AppGamingBasicJava.java
@@ -1,14 +1,15 @@
 package com.monkey_mode.learn_spring_framework;
 
 import com.monkey_mode.learn_spring_framework.game.GameRunner;
-import com.monkey_mode.learn_spring_framework.game.SuperContraGame;
+import com.monkey_mode.learn_spring_framework.game.PacmanGame;
 
 public class AppGamingBasicJava {
 
     public static void main(String[] args) {
         // var marioGame = new MarioGame();
-        var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(superContraGame);
+        // var superContraGame = new SuperContraGame();
+        var pacmanGame = new PacmanGame();
+        var gameRunner = new GameRunner(pacmanGame);
         gameRunner.run();
     }
 
