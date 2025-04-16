1b8bad69b8c8138d34e4f20f589e9b703c89bff0
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-16 22:47:18 +0700

    Retrive value of bean from context

--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/App02HelloWorldSpring.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/App02HelloWorldSpring.java
@@ -6,11 +6,16 @@ public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
         //1: Launch a Spring Context
-
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         //2: Configure the things that we want Spring to manage - @Configuration        
-        
+        //HelloWorldConfiguration - @Configuration
+        //name - @Bean
+
+        //3: Retrieve beans managed by Spring
+        System.out.println(context.getBean("name"));
+        // System.out.println(context.getBean("age"));
+
     }
 
 }
--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/HelloWorldConfiguration.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,8 +1,13 @@
 package com.monkey_mode.learn_spring_framework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
 
+    @Bean
+    public String name() {
+        return "Suphachai";
+    }
 }
