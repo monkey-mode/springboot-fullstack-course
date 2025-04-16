a25b01318f4dc258bea78965d419373ebdadb160
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-17 00:37:17 +0700

    Test spring auto wire with method call and parameters

--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/App02HelloWorldSpring.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/App02HelloWorldSpring.java
@@ -5,17 +5,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
-        //1: Launch a Spring Context
+        // 1: Launch a Spring Context
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
-        //2: Configure the things that we want Spring to manage - @Configuration        
-        //HelloWorldConfiguration - @Configuration
-        //name - @Bean
+        // 2: Configure the things that we want Spring to manage - @Configuration
+        // HelloWorldConfiguration - @Configuration
+        // name - @Bean
 
-        //3: Retrieve beans managed by Spring
+        // 3: Retrieve beans managed by Spring
         System.out.println(context.getBean("name"));
-        // System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("age"));
 
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("address2"));
+        // System.out.println(context.getBean(Address.class));
+
+        context.close();
     }
 
 }
--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/HelloWorldConfiguration.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,48 @@ package com.monkey_mode.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {
+};
+
+record Address(String street, String city) {
+};
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
-        return "Suphachai";
+        return "SuphachaiBean";
+    }
+
+    @Bean
+    public Integer age() {
+        return 20;
+    }
+
+    @Bean
+    public Person person() {
+        var person = new Person("Suphachai", 20, new Address("Main St", "Anytown"));
+        return person;
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("123 Main St", "Anytown");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Main St 3", "Anytown");
     }
 }
