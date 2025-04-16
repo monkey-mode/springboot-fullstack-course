31061abca4415924bb98a9148abbbfcfafe9ffd0
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-17 01:15:17 +0700

    Move App02 to helloworld

--- section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/helloworld/HelloWorldConfiguration.java
+++ section2/learn-spring-framework/src/main/java/com/monkey_mode/learn_spring_framework/helloworld/HelloWorldConfiguration.java
@@ -1,4 +1,4 @@
-package com.monkey_mode.learn_spring_framework;
+package com.monkey_mode.learn_spring_framework.helloworld;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
