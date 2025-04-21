0062792231a9ba5adbf79cf45641e08365747922
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-21 22:11:00 +0700

    Mongo Spring bean auto wireing

---
+++ learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/c1/DataService.java
@@ -0,0 +1,7 @@
+package com.monkey_mode.learn_spring_framework.example.c1;
+
+public interface DataService {
+
+    int[] retrieveData();
+
+}
---
+++ learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/c1/RealWorldSpringContextLauncherApplication.java
@@ -0,0 +1,17 @@
+package com.monkey_mode.learn_spring_framework.example.c1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RealWorldSpringContextLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(RealWorldSpringContextLauncherApplication.class)) {
+
+            System.out.println(context.getBean(BusinessCalculationService.class).findMax());
+        }
+    }
+}
