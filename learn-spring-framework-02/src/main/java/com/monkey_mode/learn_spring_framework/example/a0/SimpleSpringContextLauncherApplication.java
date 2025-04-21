58a4530793e0003b3f175f87171c128b81f09904
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-21 22:37:09 +0700

    Example Bean scope prototype and singleton

--- learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/a0/SimpleSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/a0/SimpleSpringContextLauncherApplication.java
@@ -5,46 +5,6 @@ import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-@Component
-class YourBusinessClass {
-    Dependency1 dependency1;
-    Dependency2 dependency2;
-
-    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
-        super();
-        System.out.println("Constructor injection - YourBusinessClass");
-        this.dependency1 = dependency1;
-        this.dependency2 = dependency2;
-    }
-
-    // @Autowired
-    // public void setDependency1(Dependency1 dependency1) {
-    // System.out.println("Setter injection - setDependency1");
-    // this.dependency1 = dependency1;
-    // }
-
-    // @Autowired
-    // public void setDependency2(Dependency2 dependency2) {
-    // System.out.println("Setter injection - setDependency2");
-    // this.dependency2 = dependency2;
-    // }
-
-    public String toString() {
-        return "Using " + dependency1 + " and " + dependency2;
-    }
-}
-
-@Component
-class Dependency1 {
-
-}
-
-@Component
-class Dependency2 {
-
-}
 
 @Configuration
 @ComponentScan
@@ -54,7 +14,6 @@ public class SimpleSpringContextLauncherApplication {
         try (var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class)) {
             Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
-            System.out.println(context.getBean(YourBusinessClass.class));
         }
     }
 }
