bbbaea13db2dc3c082d947494151af8d472e5a35
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-21 21:14:33 +0700

    Constructor injection and setter injection

--- learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/a0/SimpleSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/a0/SimpleSpringContextLauncherApplication.java
@@ -10,11 +10,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 class YourBusinessClass {
-    @Autowired
     Dependency1 dependency1;
-    
-    @Autowired
     Dependency2 dependency2;
+
+    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor injection - YourBusinessClass");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    // @Autowired
+    // public void setDependency1(Dependency1 dependency1) {
+    // System.out.println("Setter injection - setDependency1");
+    // this.dependency1 = dependency1;
+    // }
+
+    // @Autowired
+    // public void setDependency2(Dependency2 dependency2) {
+    // System.out.println("Setter injection - setDependency2");
+    // this.dependency2 = dependency2;
+    // }
 
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
