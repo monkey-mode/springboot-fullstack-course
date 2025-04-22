d1e7763723bb9ccc4d10c14cfd2b33bfe03a6867
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-22 21:35:58 +0700

    Pre and Post class

---
+++ learn-spring-framework-02/src/main/java/com/monkey_mode/learn_spring_framework/example/f1/PrePostContextLauncherApplication.java
@@ -0,0 +1,53 @@
+package com.monkey_mode.learn_spring_framework.example.f1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+class SomeClass {
+
+    private SomeDependency someDependency;
+
+    public SomeClass(SomeDependency someDependency) {
+        super();
+        this.someDependency = someDependency;
+        System.out.println("All dependencies are ready!");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        someDependency.getReady();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("Cleanup");
+    }
+}
+
+@Component
+class SomeDependency {
+
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PrePostContextLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(PrePostContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        }
+    }
+}
