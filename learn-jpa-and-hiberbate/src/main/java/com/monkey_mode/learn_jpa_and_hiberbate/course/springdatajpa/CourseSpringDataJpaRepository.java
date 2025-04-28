6e811b44798bdad0ca9596cca9cf688249b43eb7
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2025-04-28 22:33:50 +0700

    feat: Add custom query methods to CourseSpringDataJpaRepository
    - Add findByAuthor method to find courses by author
    - Add findByName method to find courses by name
    - Add findByNameAndAuthor method for combined search
    - Add findByNameOrAuthor method for flexible search

--- learn-jpa-and-hiberbate/src/main/java/com/monkey_mode/learn_jpa_and_hiberbate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ learn-jpa-and-hiberbate/src/main/java/com/monkey_mode/learn_jpa_and_hiberbate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,8 +1,19 @@
 package com.monkey_mode.learn_jpa_and_hiberbate.course.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.monkey_mode.learn_jpa_and_hiberbate.course.Course;
 
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByAuthor(String author);
+
+    List<Course> findByName(String name);
+
+    List<Course> findByNameAndAuthor(String name, String author);
+
+    List<Course> findByNameOrAuthor(String name, String author);
+
 }
---
+++ myfirstwebapp/src/test/java/com/monkey_mode/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey_mode.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
